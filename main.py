645092971f63ea2a844bad42b1c4f49fd03f9f41
werble7 <bravehenrique@gmail.com>
2022-08-13 11:46:34 -0300

    Tentei melhorar, parece que falhei

--- main.py
+++ main.py
@@ -15,115 +15,140 @@ class FilaDeProcessos:
         self.tamanho = 0
 
     def addProcesso(self, processo):
+        # vazio
         if self.primeiro is None:
 
             self.primeiro = processo
             self.ultimo = processo
 
-        elif self.primeiro == self.ultimo:
-
-            if self.primeiro.prioridade <= processo.prioridade:
-                self.ultimo.prox = processo
-                self.ultimo = processo
-
-            else:
-                processo.prox = self.primeiro
-                self.primeiro = processo
-
+        # prioridade do ultimo menor ou igual ao processo
         elif self.ultimo.prioridade <= processo.prioridade:
-
             self.ultimo.prox = processo
             self.ultimo = processo
 
-        else:
+        # um elemento
+        elif self.primeiro == self.ultimo:
 
+            # prioridade do processo de dentro menor ou igual
+            if self.primeiro.prioridade <= processo.prioridade:
+                self.primeiro.prox = processo
+                self.ultimo = processo
+
+            # prioridade do processo de fora menor
+            else:
+                self.primeiro = processo
+                processo.prox = self.ultimo
+
+        # dois ou mais
+        else:
             anterior = None
             atual = self.primeiro
-            achado = False
 
             while True:
-                if atual.prioridade == processo.prioridade + 1:
-                    processo.prox = atual
-                    if anterior is not None:
-                        anterior.prox = processo
-                    else:
+
+                # se a prioridade atual for maior, processo adicionado antes deste
+                if atual.prioridade > processo.prioridade:
+
+                    # se o anterior for o primeiro
+                    if anterior is None:
                         self.primeiro = processo
+                        processo.prox = atual
+
+                    # se não
+                    else:
+                        anterior.prox = processo
+                        processo.prox = atual
+
                     break
 
-                anterior = atual
-                if atual.prox is not None:
-                    atual = atual.prox
-
+                # se a prioridade for menor ou igual, continua o loop
                 else:
-                    self.ultimo.prox = processo
-                    self.ultimo = processo
-                    break
+                    anterior = atual
+
+                    if atual.prox is not None:
+                        atual = atual.prox
+
+                    else:
+                        break
 
         self.tamanho += 1
 
     def removeProcesso(self):
+        # verifica se está vazio
         if self.primeiro is not None:
+
+            # verifica se tem apenas um item
             if self.primeiro.prox is not None:
                 self.primeiro = self.primeiro.prox
+
             else:
                 self.primeiro = None
-            self.tamanho -= 1
 
-    def count(self):
-        return self.tamanho
+            self.tamanho -= 1
 
 
 def dekey(fila):
 
     processo = fila.primeiro.comando
 
-    if 'dekey' in processo:
-        lista = processo[8:].split()
-        x = int(lista[0])
-        if x > len(lista):
-            x = x % (len(lista) - 2)
-            if x == 0:
-                x = len(lista) - 2
-        for i in range(x):
-            if lista[1] < lista[2]:
-                lista.append(lista[1])
-                lista.pop(1)
-            else:
-                lista.append(lista[2])
-                lista.pop(2)
+    lista = processo[8:].split()
+    x = int(lista[0])
+    if x > len(lista):
 
-        for i in range(len(lista)):
-            if i != 0:
-                print(lista[i], end="")
+        x = x % (len(lista) - 2)
+        if x == 0:
+            x = len(lista) - 2
 
-        print()
-        fila.removeProcesso()
+    for i in range(x):
+
+        if lista[1] < lista[2]:
+            lista.append(lista[1])
+            lista.pop(1)
+        else:
+            lista.append(lista[2])
+            lista.pop(2)
+
+    for j in range(len(lista)):
+        if j != 0:
+            print(lista[j], end="")
+
+    print()
+    fila.removeProcesso()
 
 
 def scramble(fila):
+
     palavra = ''
     novaFrase = ''
     final = True
-    frase = fila.primeiro.comando.split()
+    frase = fila.primeiro.comando.split(" ", 2)
 
     for item in frase[2]:
+
         if item not in "()":
             palavra += item
+
         else:
+
             if palavra != '':
                 if final:
                     novaFrase += palavra
                 else:
                     novaFrase = palavra + novaFrase
+
             if item == "(":
                 final = False
+
             else:
                 final = True
+
             palavra = ''
 
     if palavra != '':
+
         if final:
             novaFrase += palavra
+
         else:
             novaFrase = palavra + novaFrase
 
@@ -153,13 +178,17 @@ if __name__ == '__main__':
                         temp = Processo(comando, int(comando[0]))
                         fila.addProcesso(temp)
 
-        elif entrada == "go":
+                else:
+                    entrada = comando
+                    break
+
+        if entrada == "go":
             if fila.primeiro is not None:
                 if 'scramble' in fila.primeiro.comando:
                     scramble(fila)
                 elif 'dekey' in fila.primeiro.comando:
                     dekey(fila)
 
-        elif entrada == "stop":
-            print(f"{fila.count()} processo(s) órfão(s).")
+        if entrada == "stop":
+            print(f"{fila.tamanho} processo(s) órfão(s).")
             break
